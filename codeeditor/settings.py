928c30bdcb8a3271b3d1ca6b701f9fc4b8a743b0
gauravgitdev <gauravkumar086421@gmail.com>
2025-05-23 06:37:38 -0700

    Update settings.py

--- codeeditor/settings.py
+++ codeeditor/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4+te_^oc=db2qrtutdzha%pe0i82zap8*%t2x)^3@v_18m4l1o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
